4eb11978b3cb6d26fd4d722baa62a6ff1e7c6f8d
GustavoBasi <gustavo.bcruz@sptech.school>
2024-11-07 18:06:00 -0300

    Exercicio Concluido

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,41 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras;
+
+    public void adicionar(Figura figura){
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas(){
+        Double soma = 0.0;
+        for (Figura figura : figuras) {
+            soma += figura.calcularArea();
+        }
+        return soma;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20(){
+        List<Figura> listaMaiorQue20 = new ArrayList<>();
+        for (Figura figura : figuras) {
+            if (figura.calcularArea() > 20) {
+                listaMaiorQue20.add(figura);
+            }
+        }
+        return listaMaiorQue20;
+    }
+
+    public List<Figura> buscarQuadrados(){
+        List<Figura> figuraQuadrada = new ArrayList<>();
+        for (Figura figura : figuras) {
+            if(figura instanceof Quadrado){
+                figuraQuadrada.add(figura);
+            }
+        }
+        return figuraQuadrada;
+    }
+
 }
